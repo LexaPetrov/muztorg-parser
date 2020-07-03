0e8770eaf5aa161c91d4223625160fccdb1d8cd4
Alexey <al1111997@yandex.ru>
2020-07-03 14:42:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const axios = require('axios')
+const cheerio = require('cheerio')
+// let link = 'https://www.muztorg.ru/category/klassicheskie-gitary?page='
+let link = 'https://www.muztorg.ru/category/ukulele?page='
+
+const parseMyztorg = async () => {
+    try {
+        let arr = []
+        let i = 1
+        let flag = false
+        while (true) {
+            console.log('step - ', i);
+            await axios.get(link + i)
+                .then(res => res.data)
+                .then(res => {
+                    let html = res
+                    $ = cheerio.load(html)
+                    let pagination = $('li.next.disabled').html()
+                    $(html).find('section.product-thumbnail').each((index, element) => {
+                        let item = {
+                            price: $(element).find('p.price').text().replace(/\s+/g, ''),
+                            name: $(element).find('div.title').text().trim(),
+                            img: $(element).find('img.img-responsive').attr('data-src')
+                        }
+
+                        arr.push(item)
+                    })                    
+                  
+                    if (pagination !== null) {
+                        flag = true
+                    }
+                })
+                .catch(err => console.log(err))
+
+
+           
+
+            if (flag) {
+                console.log(('All guitars - ', arr.length));
+                
+                fs.writeFile('muztorg.json', JSON.stringify(arr), function(err) {
+                    if(err) throw err
+                    console.log('Saved muztorg.json file');
+                })
+                break
+            }
+            i++
+        }
+    } catch (e) {
+        console.log('err - ', e);
+    }
+}
+
+parseMyztorg()
